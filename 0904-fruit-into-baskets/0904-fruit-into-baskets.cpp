87f4cff1239d96a6b1272d8d795944fbb12050e9
Krish Babbar <131786313+KrishhBabbar@users.noreply.github.com>
2026-07-21 22:42:12 +0530

    Time: 51 ms (27.74%), Space: 84.9 MB (34.1%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -3,21 +3,21 @@ public:
     int totalFruit(vector<int>& fruits) {
         unordered_map<int, int> f;
         int n = fruits.size();
-        int res = INT_MIN;
+        int res = INT_MIN; //stores result
         int high = 0;
         int low = 0;
 
         for(high = 0; high<n; high++){
-            f[fruits[high]]++;
+            f[fruits[high]]++; //add frequency of fruits at high in hashmap
             while(f.size() > 2){
-                f[fruits[low]]--;
-                if(f[fruits[low]] == 0){
+                f[fruits[low]]--; //if hashmap size is > 2 remove low 
+                if(f[fruits[low]] == 0){ //check if frequency is 0 if yes then remove from hashmap
                     f.erase(fruits[low]);
                 }
-                low++;
+                low++; //increase low
             }
-            int len = high-low+1;
-            res = max(len,res);
+            int len = high-low+1; //find len
+            res = max(len,res); //find max
         }
         return res;
     }
